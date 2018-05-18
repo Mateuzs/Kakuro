b2f4f16cfab5120bd1a2045c473453d30f9cb64e
Mateuzs <mateusz.zembol@gmail.com>
2018-05-18 10:55:13 +0200

    Intro View improvement

--- src/Views/Intro.scala
+++ src/Views/Intro.scala
@@ -4,6 +4,7 @@ import javafx.application.Application
 import javafx.scene.Scene
 import javafx.scene.control.Button
 import javafx.scene.layout.{GridPane, HBox, Priority}
+import javafx.scene.text.Text
 import javafx.stage.Stage
 
 
@@ -34,17 +35,39 @@ class Intro extends Application {
     container
   }
 
+  def createText(inputText: String): HBox ={
+
+    val text = new Text(inputText)
+    text.setId("IntroText")
+
+    val container = new HBox(text)
+    container.setId("ButtonContainer")
+
+    container
+  }
+
+
+
+
+
   def generateIntroBox(): GridPane = {
 
     val gridPane = new GridPane
 
-    gridPane.add(createButton("PLAY"), 1, 0)
-    gridPane.add(createButton("SCORES"), 1, 1)
-    gridPane.add(createButton("QUIT"), 1, 2)
+
+
+    gridPane.add(createText("KAKURO MY DEAR!"), 1, 0)
+    gridPane.add(createButton("PLAY"), 1, 1)
+    gridPane.add(createButton("SCORES"), 1, 2)
+    gridPane.add(createButton("QUIT"), 1, 3)
+    gridPane.add(createText(""), 1, 4)
+
+
 
 
     gridPane
   }
+
 
   def generateScene(gridPane: GridPane): Scene = {
 
